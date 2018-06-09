fac40136fb1bc916c695987f2bb3ac1fb76cd7a7
caibangyu <cbangyu@gmail.com>
2018-06-10 01:24:15 +0800

    fix aucompare

--- src/aucompare.c
+++ src/aucompare.c
@@ -12,17 +12,17 @@ int main(int argc, char **argv)
 {
     void *config = NULL;
     int error = 0;
-    double score = 0;
-    const char *stream_type = "PCM";
+    float score = 0;
+    const char* stream_type = "PCM";
     size_t nchannel = 1;
     size_t sample_resolution = 8;
     size_t sample_rate = 44100;
     size_t nsample = 100;
-    unsigned char *buffer = new unsigned char[int(nsample)];
+    unsigned char* buffer = new unsigned char[int(nsample)];
 
     config = new_audiohash_config();
 
-    error = set_pattern_audio(config, &stream_type, &nchannel, &sample_resolution, &sample_rate, buffer, &nsample);
+    error = set_pattern_audio(config, stream_type, &nchannel, &sample_resolution, &sample_rate, buffer, &nsample);
     if (0 != error)
     {
         fprintf(stderr, "Error occurs while setting %s\n", argv[2]);
